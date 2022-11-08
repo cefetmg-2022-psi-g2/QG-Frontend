91f4b4823e7e1799b8711c01db05443b3c0a41e4
Leônidas Braga <lebragacool@gmail.com>
2022-11-07 22:41:11 -0300

    Implementadas funcionalidades de Cadastro
    Funcionalidade de Cadastro funcionando, até onde eu testei. Falta limitar o usuário nos campos de entrada.

--- lib/pages/qg_tela_cadastro.dart
+++ lib/pages/qg_tela_cadastro.dart
@@ -1,13 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences_android/shared_preferences_android.dart';
+import 'dart:convert';
+import '../models/usuario.dart';
 
 class CadastroUsuario extends StatelessWidget {
-  const CadastroUsuario({Key? key}) : super(key: key);
+  CadastroUsuario({Key? key}) : super(key: key);
+
+  final TextEditingController controllerName = TextEditingController();
+  final TextEditingController controllerEmail = TextEditingController();
+  final TextEditingController controllerBirth = TextEditingController();
+  final TextEditingController controllerPhone = TextEditingController();
+  final TextEditingController controllerGender = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+  final TextEditingController controllerPassword2 = TextEditingController();
+
+  void login(String username, String password, BuildContext context) async {
+    var dio = Dio();
+    try {
+      Response response = await dio.post("http://10.0.2.2:4000/auth/login",
+          data: {'username': username, 'password': password});
+      if (response.statusCode == 200) {
+        String token = response.data['token'];
+        try {
+          Response userValidated = await dio.post(
+              "http://10.0.2.2:4000/auth/validate",
+              data: {'token': token});
+          if (userValidated.statusCode == 200) {
+            //passou nas 2 verificações
+            final prefs = await SharedPreferences.getInstance();
+            userValidated.data = userValidated.data['data'];
+            Usuario user = Usuario(
+                usuario: userValidated.data['username'],
+                email: userValidated.data['email'],
+                dataNascimento: userValidated.data['date_birth'],
+                telefone: userValidated.data['phone'],
+                confirmacaoSenha: 'não');
+            prefs.setString("data", jsonEncode(user));
+            prefs.setString("userToken", token);
+            Navigator.pushNamed(context, '/mainscreen');
+          }
+        } on DioError catch (e) {
+          print(e);
+        }
+      }
+    } on DioError catch (e) {
+      switch (e.response?.statusCode) {
+        case 400:
+          break;
+        case 401:
+          break;
+        case 500:
+          break;
+        default:
+          break;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 255, 251, 251),
@@ -27,11 +82,13 @@ class CadastroUsuario extends StatelessWidget {
                     iconSize: 30,
                     tooltip: 'Voltar para a tela principal',
                     onPressed: () {
-                     Navigator.pop(context);
+                      Navigator.pop(context);
                     },
                   ),
                 ),
-                SizedBox(height: 20,),
+                SizedBox(
+                  height: 20,
+                ),
                 Text(
                   "Cadastrar",
                   style: TextStyle(
@@ -41,6 +98,7 @@ class CadastroUsuario extends StatelessWidget {
                 ),
                 SizedBox(height: 20),
                 TextField(
+                  controller: controllerName,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Usuario*",
@@ -49,6 +107,7 @@ class CadastroUsuario extends StatelessWidget {
                 ),
                 SizedBox(height: 8),
                 TextField(
+                  controller: controllerEmail,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Email*",
@@ -56,6 +115,7 @@ class CadastroUsuario extends StatelessWidget {
                 ),
                 SizedBox(height: 8),
                 TextField(
+                  controller: controllerBirth,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Data de Nascimento*",
@@ -63,6 +123,7 @@ class CadastroUsuario extends StatelessWidget {
                 ),
                 SizedBox(height: 8),
                 TextField(
+                  controller: controllerPhone,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Telefone*",
@@ -70,6 +131,7 @@ class CadastroUsuario extends StatelessWidget {
                 ),
                 SizedBox(height: 8),
                 TextField(
+                  controller: controllerGender,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Sexo",
@@ -77,6 +139,7 @@ class CadastroUsuario extends StatelessWidget {
                 ),
                 SizedBox(height: 8),
                 TextField(
+                  controller: controllerPassword,
                   obscureText: true,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
@@ -85,13 +148,16 @@ class CadastroUsuario extends StatelessWidget {
                 ),
                 SizedBox(height: 8),
                 TextField(
+                  controller: controllerPassword2,
                   obscureText: true,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Confirmação da Senha*',
                   ),
                 ),
-                SizedBox(height: 30,),
+                SizedBox(
+                  height: 30,
+                ),
                 Column(
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -103,24 +169,79 @@ class CadastroUsuario extends StatelessWidget {
                         fontWeight: FontWeight.w100,
                       ),
                     ),
-                    SizedBox(height: 15,),
+                    SizedBox(
+                      height: 15,
+                    ),
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         primary: Color(0xff1FFFBF),
                       ),
-                      onPressed: () {
+                      onPressed: () async {
+                        var dio = Dio();
 
+                        if (controllerName.text.isEmpty &&
+                            controllerBirth.text.isEmpty &&
+                            controllerEmail.text.isEmpty &&
+                            controllerPhone.text.isEmpty &&
+                            controllerPassword.text.isEmpty &&
+                            controllerPassword2.text.isEmpty) {
+                          return;
+                        }
+                        if (controllerPassword.text !=
+                            controllerPassword2.text) {
+                          const snackBar =
+                              SnackBar(content: Text("As devem ser iguais."));
+                          ScaffoldMessenger.of(context).clearSnackBars();
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                        try {
+                          Response response = await dio
+                              .post("http://10.0.2.2:4000/auth/signup", data: {
+                            'name': controllerName.text,
+                            'username': controllerName.text,
+                            'password': controllerPassword.text,
+                            'email': controllerEmail.text,
+                            'dateBirth': controllerBirth.text,
+                            'phone': controllerPhone.text,
+                            'gender': controllerGender.text
+                          });
+                          if (response.statusCode == 200) {
+                            // ignore: use_build_context_synchronously
+                            login(controllerName.text, controllerPassword.text,
+                                context);
+                          }
+                        } on DioError catch (e) {
+                          switch (e.response?.statusCode) {
+                            case 400:
+                              const snackBar = SnackBar(
+                                  content:
+                                      Text("Dados invalidos ou incorretos."));
+                              ScaffoldMessenger.of(context).clearSnackBars();
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                              break;
+                            case 500:
+                              const snackBar = SnackBar(
+                                  content: Text(
+                                      "Erro do servidor. Por favor tente novamente mais tarde."));
+                              ScaffoldMessenger.of(context).clearSnackBars();
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                              break;
+                            default:
+                              break;
+                          }
+                        }
                       },
                       child: const Text(
                         'Realizar Cadastro',
                         style: TextStyle(
                           color: Colors.black,
                         ),
-                        ),
+                      ),
                     ),
                   ],
                 ),
-    
               ],
             ),
           ),
