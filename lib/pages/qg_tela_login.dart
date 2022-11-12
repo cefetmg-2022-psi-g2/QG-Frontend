1c9892cf99191216134d5c48a328ff861a0cee7d
luizfernando103 <79534453+luizfernando103@users.noreply.github.com>
2022-11-12 19:13:46 -0300

    Co-authored-by: Vitor Freitas <VitorExl@users.noreply.github.com>

--- lib/pages/qg_tela_login.dart
+++ lib/pages/qg_tela_login.dart
@@ -17,196 +17,193 @@ class LoginUsuario extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        margin: EdgeInsets.all(15),
-        child: Scaffold(
-          backgroundColor: Color.fromARGB(255, 255, 251, 251),
-          body: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Color(0xff1FFFBF),
-                          child: IconButton(
-                            icon: const Icon(Icons.arrow_back_ios_rounded),
-                            color: Colors.black,
-                            iconSize: 30,
-                            tooltip: 'Voltar para a tela principal',
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 251, 251),
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xff1FFFBF),
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_back_ios_rounded),
+                          color: Colors.black,
+                          iconSize: 30,
+                          tooltip: 'Voltar para a tela principal',
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
                         ),
-                        SizedBox(
-                          height: 20,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Entrar",
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.w700,
                         ),
-                        Text(
-                          "Entrar",
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.w700,
-                          ),
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        controller: usernameController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Username",
                         ),
-                        SizedBox(height: 20),
-                        TextFormField(
-                          controller: usernameController,
+                        validator: (text) {
+                          if (text == null || text.isEmpty)
+                            return "Nome inválido!";
+                        },
+                      ),
+                      SizedBox(height: 8),
+                      TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
                           decoration: const InputDecoration(
                             border: OutlineInputBorder(),
-                            labelText: "Username",
+                            labelText: 'Senha*',
                           ),
                           validator: (text) {
-                            if (text == null || text.isEmpty)
-                              return "Nome inválido!";
-                          },
+                            if (text == null ||
+                                (text.isEmpty || text.length < 6))
+                              return "Senha inválida!";
+                          }),
+                      SizedBox(
+                        height: 60,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xff1FFFBF),
                         ),
-                        SizedBox(height: 8),
-                        TextFormField(
-                            controller: passwordController,
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Senha*',
-                            ),
-                            validator: (text) {
-                              if (text == null ||
-                                  (text.isEmpty || text.length < 6))
-                                return "Senha inválida!";
-                            }),
-                        SizedBox(
-                          height: 60,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Color(0xff1FFFBF),
-                          ),
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              var dio = Dio();
-                              try {
-                                Response response = await dio.post(
-                                    "http://10.0.2.2:4000/auth/login",
-                                    data: {
-                                      'username': usernameController.text,
-                                      'password': passwordController.text
-                                    });
-                                if (response.statusCode == 200) {
-                                  String token = response.data['token'];
-                                  try {
-                                    Response userValidated = await dio.post(
-                                        "http://10.0.2.2:4000/auth/validate",
-                                        data: {'token': token});
-                                    if (userValidated.statusCode == 200) {
-                                      //passou nas 2 verificações
-                                      final prefs =
-                                          await SharedPreferences.getInstance();
-                                      userValidated.data =
-                                          userValidated.data['data'];
-                                      Usuario user = Usuario(
-                                          usuario:
-                                              userValidated.data['username'],
-                                          email: userValidated.data['email'],
-                                          dataNascimento:
-                                              userValidated.data['date_birth'],
-                                          telefone: userValidated.data['phone'],
-                                          confirmacaoSenha: 'não');
-                                      prefs.setString("data", jsonEncode(user));
-                                      prefs.setString("userToken", token);
-                                      Navigator.pushNamed(
-                                          context, '/mainscreen');
-                                    }
-                                  } on DioError catch (e) {
-                                    print(e);
-                                    const snackBar = SnackBar(
-                                        content:
-                                            Text("Erro interno do gateway"));
-                                    ScaffoldMessenger.of(context)
-                                        .clearSnackBars();
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(snackBar);
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            var dio = Dio();
+                            try {
+                              Response response = await dio.post(
+                                  "http://10.0.2.2:4000/auth/login",
+                                  data: {
+                                    'username': usernameController.text,
+                                    'password': passwordController.text
+                                  });
+                              if (response.statusCode == 200) {
+                                String token = response.data['token'];
+                                try {
+                                  Response userValidated = await dio.post(
+                                      "http://10.0.2.2:4000/auth/validate",
+                                      data: {'token': token});
+                                  if (userValidated.statusCode == 200) {
+                                    //passou nas 2 verificações
+                                    final prefs =
+                                        await SharedPreferences.getInstance();
+                                    userValidated.data =
+                                        userValidated.data['data'];
+                                    Usuario user = Usuario(
+                                        usuario:
+                                            userValidated.data['username'],
+                                        email: userValidated.data['email'],
+                                        dataNascimento:
+                                            userValidated.data['date_birth'],
+                                        telefone: userValidated.data['phone'],
+                                        confirmacaoSenha: 'não');
+                                    prefs.setString("data", jsonEncode(user));
+                                    prefs.setString("userToken", token);
+                                    Navigator.pushNamed(
+                                        context, '/mainscreen');
                                   }
-                                }
-                              } on DioError catch (e) {
-                                switch (e.response?.statusCode) {
-                                  case 400:
-                                    passwordController.clear();
-                                    usernameController.clear();
-                                    const snackBar = SnackBar(
-                                        content: Text(
-                                            "Dados invalidos ou incorretos."));
-                                    ScaffoldMessenger.of(context)
-                                        .clearSnackBars();
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(snackBar);
-                                    break;
-                                  case 401:
-                                    passwordController.clear();
-                                    usernameController.clear();
-                                    const snackBar = SnackBar(
-                                        content: Text(
-                                            "Usuário e/ou senha incorretos."));
-                                    ScaffoldMessenger.of(context)
-                                        .clearSnackBars();
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(snackBar);
-                                    break;
-                                  case 500:
-                                    passwordController.clear();
-                                    usernameController.clear();
-                                    const snackBar = SnackBar(
-                                        content: Text(
-                                            "Erro do servidor. Por favor tente novamente mais tarde."));
-                                    ScaffoldMessenger.of(context)
-                                        .clearSnackBars();
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(snackBar);
-                                    break;
-                                  default:
-                                    break;
+                                } on DioError catch (e) {
+                                  print(e);
+                                  const snackBar = SnackBar(
+                                      content:
+                                          Text("Erro interno do gateway"));
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(snackBar);
                                 }
                               }
-                              ;
+                            } on DioError catch (e) {
+                              switch (e.response?.statusCode) {
+                                case 400:
+                                  passwordController.clear();
+                                  usernameController.clear();
+                                  const snackBar = SnackBar(
+                                      content: Text(
+                                          "Dados invalidos ou incorretos."));
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(snackBar);
+                                  break;
+                                case 401:
+                                  passwordController.clear();
+                                  usernameController.clear();
+                                  const snackBar = SnackBar(
+                                      content: Text(
+                                          "Usuário e/ou senha incorretos."));
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(snackBar);
+                                  break;
+                                case 500:
+                                  passwordController.clear();
+                                  usernameController.clear();
+                                  const snackBar = SnackBar(
+                                      content: Text(
+                                          "Erro do servidor. Por favor tente novamente mais tarde."));
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(snackBar);
+                                  break;
+                                default:
+                                  break;
+                              }
                             }
-                          },
-                          child: const Text(
-                            'Realizar Login',
+                            ;
+                          }
+                        },
+                        child: const Text(
+                          'Realizar Login',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Esqueci Minha Senha",
                             style: TextStyle(
-                              color: Colors.black,
+                              fontWeight: FontWeight.w200,
+                              fontSize: 15,
                             ),
+                            textAlign: TextAlign.left,
                           ),
+                          
                         ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: FlatButton(
-                            onPressed: () {},
-                            child: Text(
-                              "Esqueci Minha Senha",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w200,
-                                fontSize: 15,
-                              ),
-                              textAlign: TextAlign.left,
-                            ),
-                            
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
           ),
--- lib/widgets/card-pedidos.dart
+++ lib/widgets/card-pedidos.dart
@@ -11,66 +11,109 @@ class CardPedido extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.grey[300],
-      ),
-      padding: EdgeInsets.all(8),
-      margin: EdgeInsets.all(10),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(
-            Icons.account_circle_rounded,
-            size: 100,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            pedido.item,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
+    return Column(
+      children: [
+        FlatButton(
+          onPressed: () {
+            print("a");
+          },
+          child: Container(
+            alignment: Alignment.centerLeft,
+            width: 195,
+            height: 120,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Color(0xffE7E7E7),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(-6, 8),
+                    spreadRadius: -11,
+                    blurRadius: 14,
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                  ),
+                ]),
+            padding: EdgeInsets.fromLTRB(12, 0, 12, 12),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      child: Icon(
+                        Icons.account_circle_rounded,
+                        size: 60,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Usuário",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Color(0xff1FFFBF),
+                              size: 11,
+                            ),
+                            Text(
+                              "4,5",
+                              style: TextStyle(
+                                fontSize: 11,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Carregador tipo C",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: 8,
+                    ),
+                    Text(
+                      "C2-P2 - Segundo Andar, sala 207",
+                      style: TextStyle(
+                        fontSize: 8,
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
-          SizedBox(
-            height: 2,
-          ),
-          Text(
-            "Campus - ${pedido.campus.toString()}",
-            style: TextStyle(
-              fontSize: 12,
-            ),
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          Text(
-            "Prédio - ${pedido.predio}",
-            style: TextStyle(
-              fontSize: 12,
-            ),
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          Text(
-            "Complemento - ${pedido.complemento}",
-            style: TextStyle(
-              fontSize: 12,
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Atender Pedido"),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
